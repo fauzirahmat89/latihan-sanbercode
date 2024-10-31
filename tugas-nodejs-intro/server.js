e0afa829e9f08d54294ad152f44151afa993d5c5
fauzirahmat89 <fauzirahmat89@gmail.com>
2024-10-31 20:17:04 +0700

    express to http

--- tugas-nodejs-intro/server.js
+++ tugas-nodejs-intro/server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
 
-app.get('/', (req,res) => {
-    res.send('Hello, World!')
-})
+const server = http.createServer((req, res) => {
+  
+  setTimeout(() => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+  }, 1000);
+});
 
-app.listen(port, ()=>{
-    console.log(`server is running at http://localhost:${port}`)
-})
+server.listen(3000, () => {
+  console.log('Server running at <http://localhost:3000/>');
+});
