d61cfe2c9d68106a71fcbbea1f1bd8fccfddaac2
fauzirahmat89 <fauzirahmat89@gmail.com>
2024-11-04 22:03:33 +0700

    Menambahkan tugas framework express

---
+++ tugas-framework-express/src/index.ts
@@ -0,0 +1,45 @@
+import express, { Request, response, Response } from "express";
+import path from "path";
+const PORT = 3000;
+
+function init() {
+  const app = express();
+
+  // Melayani file statis dari direktori "public"
+  app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+  app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+      message: "OK",
+      data: null,
+    });
+  });
+
+  app.get('/hello',(req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Success fetch message",
+        data: "Hello World!"
+    });
+  });
+
+  app.get('/user', (req: Request,res:Response)=>{
+    res.status(200).json({
+      message: "Success fetch user",
+      data: {
+        id: 1,
+        name: "Budi",
+        username: "budidu",
+        email: "budidu@mail.com"
+      }
+    });
+  });
+    
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+init();
